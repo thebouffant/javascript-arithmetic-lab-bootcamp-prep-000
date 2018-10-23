484ed03b439f3d485c5910080dd37e01da151506
Marc <marc.troughton@gmail.com>
2018-10-23 02:20:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
-var number
+
 function add(a,b) {
+  var number
   number = a+b
   return number
 }
 function subtract(a,b) {
+  var number
   number = a-b 
   return number
 }
 function multiply(a,b) {
+  var number
   number = a*b
   return number
 }
 function divide(a,b) {
+  var number
   number = a/b 
   return number
 }
 function inc(n) {
+  var number
   number +=n
   return number
 }
 function dec(n) {
+  var number
   number =-n 
   return number
 }
