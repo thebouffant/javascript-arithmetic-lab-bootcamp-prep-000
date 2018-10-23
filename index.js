b291dfb81cb89d7c499eae5443f086cbfefbd1c9
Marc <marc.troughton@gmail.com>
2018-10-23 02:18:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,12 @@ function subtract(a,b) {
   number = a-b 
   return number
 }
-
+function multiply(a,b) {
+  number = a*b
+  return number
+}
+function divide(a,b) {
+  number = a/b 
+  return number
+}
 
